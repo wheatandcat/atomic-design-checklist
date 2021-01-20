6cf5269a171603d8f4e423a1a430e8c1b28403c3
wheatandcat <wheatandcat@gmail.com>
2021-01-20 19:56:04 +0900

    fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  purge: {
+    content: [
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./pages/index.tsx",
+    ],
+    options: {
+      safelist: [
+        "w-24",
+        "h-24",
+        "w-32",
+        "h-32",
+        "w-64",
+        "h-64",
+        "w-96",
+        "h-96",
+      ],
+    },
+  },
   darkMode: "media", // 'media' or 'class'
   theme: {
     extend: {},
